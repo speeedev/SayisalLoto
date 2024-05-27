4a674ee270f0d69434471ef5da91452231bb844c
Emir Bolat <89213834+speeedev@users.noreply.github.com>
2024-05-27 23:32:54 +0300

    Update SayisalLoto.dart

--- SayisalLoto.dart
+++ SayisalLoto.dart
@@ -3,32 +3,47 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class SayisalLoto extends StatefulWidget {
-  const SayisalLoto({super.key});
-
-  @override
-  State<SayisalLoto> createState() => _SayisalLotoState();
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NumericalLottery(),
+    ),
+  );
 }
 
-int cevap = 0;
+class NumericalLottery extends StatefulWidget {
+  const NumericalLottery({super.key});
 
-class _SayisalLotoState extends State<SayisalLoto> {
+  @override
+  State<NumericalLottery> createState() => _NumericalLotteryState();
+}
+
+int answer = 0;
+
+class _NumericalLotteryState extends State<NumericalLottery> {
   Random random = Random();
 
-  num bakiye = 10;
-  int kazanilanPara = 0;
-  List<int> sistem = [];
-  List<int> oyuncu = [];
-  String sayicontrol = "";
-  var sayi1 = 0, sayi2 = 0, sayi3 = 0, sayi4 = 0, sayi5 = 0, sayi6 = 0;
+  num balance = 10;
+  int wonMoney = 0;
+  List<int> systemNumbers = [];
+  List<int> playerNumbers = [];
+  String numberValidation = "";
+  var number1 = 0,
+      number2 = 0,
+      number3 = 0,
+      number4 = 0,
+      number5 = 0,
+      number6 = 0;
 
-  final sayi1Controller = TextEditingController();
-  final sayi2Controller = TextEditingController();
-  final sayi3Controller = TextEditingController();
-  final sayi4Controller = TextEditingController();
-  final sayi5Controller = TextEditingController();
-  final sayi6Controller = TextEditingController();
-  playerNumbersRandom() {
+  final number1Controller = TextEditingController();
+  final number2Controller = TextEditingController();
+  final number3Controller = TextEditingController();
+  final number4Controller = TextEditingController();
+  final number5Controller = TextEditingController();
+  final number6Controller = TextEditingController();
+
+  void generatePlayerNumbersRandom() {
     List<int> numbers = [];
     Random rand = Random();
     while (numbers.length < 6) {
@@ -37,18 +52,18 @@ class _SayisalLotoState extends State<SayisalLoto> {
         numbers.add(num);
       }
     }
-    sayi1Controller.text = numbers[0].toString();
-    sayi2Controller.text = numbers[1].toString();
-    sayi3Controller.text = numbers[2].toString();
-    sayi4Controller.text = numbers[3].toString();
-    sayi5Controller.text = numbers[4].toString();
-    sayi6Controller.text = numbers[5].toString();
+    number1Controller.text = numbers[0].toString();
+    number2Controller.text = numbers[1].toString();
+    number3Controller.text = numbers[2].toString();
+    number4Controller.text = numbers[3].toString();
+    number5Controller.text = numbers[4].toString();
+    number6Controller.text = numbers[5].toString();
     setState(() {
-      oyuncu = numbers;
+      playerNumbers = numbers;
     });
   }
 
-  systemNumbersRandom() {
+  void generateSystemNumbersRandom() {
     List<int> numbers = [];
     Random rand = Random();
     while (numbers.length < 6) {
@@ -58,35 +73,35 @@ class _SayisalLotoState extends State<SayisalLoto> {
       }
     }
     setState(() {
-      sistem = numbers;
+      systemNumbers = numbers;
     });
   }
 
-  void odulSistemi() {
-    switch (cevap) {
+  void rewardSystem() {
+    switch (answer) {
       case 1:
-        kazanilanPara = 10;
-        bakiye += kazanilanPara;
+        wonMoney = 10;
+        balance += wonMoney;
         break;
       case 2:
-        kazanilanPara = 15;
-        bakiye += kazanilanPara;
+        wonMoney = 15;
+        balance += wonMoney;
         break;
       case 3:
-        kazanilanPara = 20;
-        bakiye += kazanilanPara;
+        wonMoney = 20;
+        balance += wonMoney;
         break;
       case 4:
-        kazanilanPara = 30;
-        bakiye += kazanilanPara;
+        wonMoney = 30;
+        balance += wonMoney;
         break;
       case 5:
-        kazanilanPara = 40;
-        bakiye += kazanilanPara;
+        wonMoney = 40;
+        balance += wonMoney;
         break;
       case 6:
-        kazanilanPara = 100;
-        bakiye += kazanilanPara;
+        wonMoney = 100;
+        balance += wonMoney;
         break;
       default:
     }
@@ -96,195 +111,191 @@ class _SayisalLotoState extends State<SayisalLoto> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sayisal Loto"),
+        title: const Text("Numerical Lottery"),
         actions: [
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                bakiye.toString(),
-                style: TextStyle(fontSize: 25),
+                balance.toString(),
+                style: const TextStyle(fontSize: 25),
               ),
             ],
           ),
-          SizedBox(width: 10),
+          const SizedBox(width: 10),
         ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  inputFormatters: [
-                    CustomRangeTextInputFormatter(),
+      body: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      inputFormatters: [
+                        CustomRangeTextInputFormatter(),
+                      ],
+                      keyboardType: TextInputType.number,
+                      controller: number1Controller,
+                      decoration: const InputDecoration(
+                        hintText: "1. Number",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: number2Controller,
+                      inputFormatters: [
+                        CustomRangeTextInputFormatter(),
+                      ],
+                      decoration: const InputDecoration(
+                        hintText: "2. Number",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: number3Controller,
+                      inputFormatters: [
+                        CustomRangeTextInputFormatter(),
+                      ],
+                      decoration: const InputDecoration(
+                        hintText: "3. Number",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: number4Controller,
+                      inputFormatters: [
+                        CustomRangeTextInputFormatter(),
+                      ],
+                      decoration: const InputDecoration(
+                        hintText: "4. Number",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: number5Controller,
+                      inputFormatters: [
+                        CustomRangeTextInputFormatter(),
+                      ],
+                      decoration: const InputDecoration(
+                        hintText: "5. Number",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: number6Controller,
+                      inputFormatters: [
+                        CustomRangeTextInputFormatter(),
+                      ],
+                      decoration: const InputDecoration(
+                        hintText: "6. Number",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ],
-                  keyboardType: TextInputType.number,
-                  controller: sayi1Controller,
-                  decoration: const InputDecoration(
-                    hintText: "1. Sayı",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  controller: sayi2Controller,
-                  inputFormatters: [
-                    CustomRangeTextInputFormatter(),
-                  ],
-                  decoration: const InputDecoration(
-                    hintText: "2. Sayı",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  controller: sayi3Controller,
-                  inputFormatters: [
-                    CustomRangeTextInputFormatter(),
-                  ],
-                  decoration: const InputDecoration(
-                    hintText: "3. Sayı",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  controller: sayi4Controller,
-                  inputFormatters: [
-                    CustomRangeTextInputFormatter(),
-                  ],
-                  decoration: const InputDecoration(
-                    hintText: "4. Sayı",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  controller: sayi5Controller,
-                  inputFormatters: [
-                    CustomRangeTextInputFormatter(),
-                  ],
-                  decoration: const InputDecoration(
-                    hintText: "5. Sayı",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  controller: sayi6Controller,
-                  inputFormatters: [
-                    CustomRangeTextInputFormatter(),
-                  ],
-                  decoration: const InputDecoration(
-                    hintText: "6. Sayı",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    playerNumbersRandom();
-                  });
-                },
-                child: Text(
-                  "Rastgele Sayılar Üret",
-                  style: TextStyle(fontSize: 20),
                 ),
               ),
-            ),
-          ),
-          SizedBox(height: 8),
-          Padding(
-            padding: EdgeInsets.only(left: 10, right: 10),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (bakiye == 0) {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        content: Text("Bakiyen yetersiz"),
-                      ),
-                    );
-                  } else if (oyuncu.isEmpty) {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        content: Text("Altı tane sayı girmeniz gerekiyor"),
-                      ),
-                    );
-                  } else {
-                    oyuncu = [
-                      int.parse(sayi1Controller.text),
-                      int.parse(sayi2Controller.text),
-                      int.parse(sayi3Controller.text),
-                      int.parse(sayi4Controller.text),
-                      int.parse(sayi5Controller.text),
-                      int.parse(sayi6Controller.text),
-                    ];
-                    systemNumbersRandom();
-                    cevap = 0;
-                    for (int i = 0; i < 6; i++) {
-                      if (oyuncu[i] == sistem[i]) {
-                        print("sistem:" + sistem.toString());
-                        cevap = cevap + 1;
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        generatePlayerNumbersRandom();
+                      });
+                    },
+                    child: const Text(
+                      "Generate Random Numbers",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (balance == 0) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => const AlertDialog(
+                            content: Text("Insufficient balance"),
+                          ),
+                        );
+                      } else if (playerNumbers.isEmpty) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => const AlertDialog(
+                            content: Text("You need to enter six numbers"),
+                          ),
+                        );
+                      } else {
+                        playerNumbers = [
+                          int.parse(number1Controller.text),
+                          int.parse(number2Controller.text),
+                          int.parse(number3Controller.text),
+                          int.parse(number4Controller.text),
+                          int.parse(number5Controller.text),
+                          int.parse(number6Controller.text),
+                        ];
+                        generateSystemNumbersRandom();
+                        answer = 0;
+                        for (int i = 0; i < 6; i++) {
+                          if (playerNumbers[i] == systemNumbers[i]) {
+                            print("system:" + systemNumbers.toString());
+                            answer = answer + 1;
+                          }
+                        }
+                        print("Correct number:" + answer.toString());
+                        rewardSystem();
+                        setState(() {
+                          balance--;
+                        });
                       }
-                    }
-                    print("Doğru sayısı:" + cevap.toString());
-                    odulSistemi();
-                    setState(() {
-                      bakiye--;
-                    });
-                  }
-                },
-                child: Text(
-                  "Oyna",
-                  style: TextStyle(fontSize: 20),
+                    },
+                    child: const Text(
+                      "Play",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
                 ),
               ),
-            ),
+              const SizedBox(height: 25),
+              Text(
+                "Player: " + playerNumbers.toString(),
+                style: const TextStyle(fontSize: 22),
+              ),
+              Text(
+                "System: " + systemNumbers.toString(),
+                style: const TextStyle(fontSize: 22),
+              ),
+              Text(
+                "Correct numbers: " + answer.toString(),
+                style: const TextStyle(fontSize: 22),
+              ),
+            ],
           ),
-          SizedBox(height: 25),
-          Text(
-            "Kullanıcı: " + oyuncu.toString(),
-            style: TextStyle(fontSize: 22),
-          ),
-          Text(
-            "Sistem: " + sistem.toString(),
-            style: TextStyle(fontSize: 22), // arttı para
-          ),
-          Text(
-            "Doğru sayısı: " + cevap.toString(),
-            style: TextStyle(fontSize: 22),
-          ),
-        ],
+        ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
 
 class CustomRangeTextInputFormatter extends TextInputFormatter {
   @override
